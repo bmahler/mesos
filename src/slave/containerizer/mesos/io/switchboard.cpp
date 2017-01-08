12760f8fbe6e54af0f7a4288bdd1cc17c8c554a1
Benjamin Hindman <benjamin.hindman@gmail.com>
2017-01-07 23:05:15 -0800

    Used Future::recover in Mesos.
    Review: https://reviews.apache.org/r/61151

--- src/slave/containerizer/mesos/io/switchboard.cpp
+++ src/slave/containerizer/mesos/io/switchboard.cpp
@@ -1634,12 +1634,7 @@ Future<http::Response> IOSwitchboardServerProcess::attachContainerInput(
                   -> ControlFlow<http::Response> {
                 return Continue();
               }))
-              // TODO(benh):
-              // .recover(defer(self(), [=](...) {
-              //   failure = Failure("Failed writing to stdin: discarded");
-              //   return Break(http::InternalServerError(failure->message));
-              // }))
-              .repair(defer(self(), [=](
+              .recover(defer(self(), [=](
                   const Future<ControlFlow<http::Response>>& future)
                   -> ControlFlow<http::Response> {
                 failure = Failure(
