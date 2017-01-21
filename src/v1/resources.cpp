ab699181fbbf231496bb578b07ed3e9d786efb4b
Benjamin Mahler <bmahler@apache.org>
2017-01-20 18:31:45 -0800

    Augmented printing of Resources to include AllocationInfo.
    Ignoring whether there is a disk info, the format is as follows:
    name(reservedrole[, principal[, labels]])(allocatedrole):value
    
    Review: https://reviews.apache.org/r/55825

--- src/v1/resources.cpp
+++ src/v1/resources.cpp
@@ -1938,6 +1938,10 @@ ostream& operator<<(ostream& stream, const Resource& resource)
 
   stream << ")";
 
+  if (resource.has_allocation_info()) {
+    stream << "(allocated: " << resource.allocation_info().role() << ")";
+  }
+
   if (resource.has_disk()) {
     stream << "[" << resource.disk() << "]";
   }
